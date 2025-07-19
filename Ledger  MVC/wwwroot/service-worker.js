5d353922a91be589dd8dcdc5a970784788838885
Assem Khashaba <assemkhashaba0@gmail.com>
2025-07-19 18:12:04 +0300

    PWA

--- Ledger  MVC/wwwroot/service-worker.js
+++ Ledger  MVC/wwwroot/service-worker.js
@@ -1,12 +1,53 @@
-﻿self.addEventListener('install', function (event) {
-    console.log('Service Worker installing.');
-    self.skipWaiting();
+﻿const CACHE_NAME = 'daftar-v1.0.1';
+const urlsToCache = [
+  '/',
+  '/css/site.css',
+  '/js/site.js',
+  '/IMG/ASVS Logo.png',
+  '/manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800&display=swap'
+];
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Caching files');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', function (event) {
-    console.log('Service Worker activating.');
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activating.');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(fetch(event.request));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
